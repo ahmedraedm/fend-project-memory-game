77e3e736f58cc98a70e2b8461f4f27cb0031ffb8
Ahmed Raed Moukhtar <ahmedraedm@outlook.com>
2018-03-25 11:16:23 +0200

    Update1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
+const li = $('li');
+let itemName,itemOne,itemTwo,counter,openedCards,totMatchedCtr;
 /*
  * Create a list that holds all of your cards
  */
 
-
+// Initializing function to remove any list item hasClass('open','show','match'), empty 'openedCards' array, set 'counter' & 'totMatchedCtr' to zero
+$(document).ready(function() {
+    $('.open, .show, .match').removeClass('open show match');
+    openedCards =[];
+    counter=0;
+    totMatchedCtr=0;
+    document.getElementById("spanCounter").innerHTML = counter;
+});
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -25,6 +34,58 @@ function shuffle(array) {
     return array;
 }
 
+function addCounter(){
+  counter += 1;
+  document.getElementById("spanCounter").innerHTML = counter;
+  debugger;
+}
+
+function showCard(e){
+  $(e.target).addClass('open');
+  $(e.target).addClass('show');
+}
+
+function match(){
+  $('.open').addClass('match');
+  $('.open').removeClass('open show');
+  totMatchedCtr += 2;
+}
+
+function notMatch(e){
+  $('.open').removeClass('open show');
+};
+
+// This function sets the name of the second class of the first child (item) of the clicked List to variable "itemName", then push it to the array opnedCards
+function addCardToArr(e){
+  itemName=$(e.target.children[0]).attr('class').split(' ')[1];
+  openedCards.push(itemName);
+  if (openedCards.length==2) {
+    itemOne=openedCards[0];
+    itemTwo=openedCards[1];
+    if (itemOne===itemTwo) {
+        match();
+    }
+    else{
+      // debugger;
+      setTimeout(notMatch,700)
+    }
+    openedCards=[];
+    addCounter();
+  }
+}
+
+// Event listener when clicking on a list item
+li.on('click', function (evt) {
+    // Checks whether this list item already has a match class or not.
+    if (!$(evt.target).hasClass('match')) {
+      showCard(evt);
+      addCardToArr(evt);
+      if (totMatchedCtr===16) {
+        alert("You win, your score is: "+ counter);
+      }
+    }
+    // debugger;
+})
 
 /*
  * set up the event listener for a card. If a card is clicked:

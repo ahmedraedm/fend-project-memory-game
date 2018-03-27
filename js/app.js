f55ffb29486b3eb6d176e5ec9e907b7d48874037
Ahmed Raed Moukhtar <ahmedraedm@outlook.com>
2018-03-27 14:57:26 +0200

    update3

--- js/app.js
+++ js/app.js
@@ -1,23 +1,33 @@
 const li = $('li');
-let itemName,itemOne,itemTwo,counter,openedCards,totMatchedCtr,cardsShuffled,checkOpen;
+let itemName,itemOne,itemTwo,counter,openedCards,totMatchedCtr,cardsShuffled,checkOpen,stars;
 const cardShapes =['fa-diamond' ,'fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-anchor','fa-leaf','fa-bicycle','fa-diamond','fa-bomb','fa-leaf','fa-bomb','fa-bolt','fa-bicycle','fa-paper-plane-o','fa-cube'];
 
-// Initializing function to remove any list item hasClass('open','show','match'), empty 'openedCards' array, set 'counter' & 'totMatchedCtr' to zero
 $(document).ready(function() {
   initialize();
 });
 
+// Initializing function to remove any list item hasClass('open','show','match'), empty 'openedCards' array, set 'counter' & 'totMatchedCtr' to zero
+//openedCards: array contains the class name of any opened card <i>
+//counter: Counts number of moves.
+//totMatchedCtr: Total Matched Counter, it is incremented by 2 for every 2 cards matched.
+//cardShapes: Array contains all cards shapes (classes)
 function initialize() {
   $('.open, .show, .match').removeClass('open show match');
   openedCards =[];
   counter=0;
   totMatchedCtr=0;
+  stars=3;
   document.getElementById("spanCounter").innerHTML = counter;
   cardsShuffled = shuffle(cardShapes);
   // adding card shape class to each <i> element
   for (var i = 0; i < cardsShuffled.length; i++) {
+    $('.deck').children('li').children('i').eq(i).attr('class','fa') // remove any class from the last game except 'fa' class
     $('.deck').children('li').children('i').eq(i).addClass(cardsShuffled[i])
   }
+  for (var i = 0; i <=2; i++) {
+    $('.stars').children('li').children('i').eq(i).attr('class','fa fa-star') // remove any class from the last game except 'fa' class
+  }
+  $('.game-end').removeClass('game-end-show');
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -37,8 +47,18 @@ function shuffle(array) {
 function addCounter(){
   counter += 1;
   document.getElementById("spanCounter").innerHTML = counter;
-  // debugger;
-}
+  switch (true) {
+    case counter>10 && counter<=14:
+      $('.stars').children('li').children('i').eq(1).attr('class','fa') // remove any class from the last game except 'fa' class
+      stars =2;
+      break;
+    case counter>15:
+      $('.stars').children('li').children('i').eq(2).attr('class','fa') // remove any class from the last game except 'fa' class
+      stars =1;
+      break;
+    default:
+    stars=3;
+}}
 
 function showCard(e){
   $(e.target).addClass('open show');
@@ -58,7 +78,6 @@ function notMatch(e){
 
 // This function sets the name of the second class of the first child (item) of the clicked List to variable "itemName", then push it to the array opnedCards
 function addCardToArr(e){
-  debugger;
   if (e.target.localName==="li") {
     itemName=$(e.target.children[0]).attr('class').split(' ')[1];
   }
@@ -86,8 +105,10 @@ function addCardToArr(e){
   }
 }
 
-//  Checks if the clicked element (whether it is <li> element or <i> element) already opened before or not(whether has class "open")
+//  Checks if the clicked element (whether it is <li> element or <i> element) already opened before or not.
+// checkOpen: Is true when the clicked element already opened before(has a class'open')
 function hasClassOpen(evt) {
+  // Clicked element may be <i> or <li>
   if (evt.target.localName==="li") {
     checkOpen=$(evt.target).hasClass('open');
   }
@@ -96,37 +117,32 @@ function hasClassOpen(evt) {
   }
 }
 
+function checkIfWin(){
+  if (totMatchedCtr===16) { // All cards matched. Win!
+    $('.game-end').addClass('game-end-show'); // show the celebrate background
+    setTimeout(function(){  // Wait untill the celebration background appears
+      let answer =confirm(`Congratulations! You won, your score is: ${counter} and stars: ${stars}
+Play again?`);
+  if (answer) {
+    initialize();
+  }
+    },500);
+  }
+}
+
 // Event listener when clicking on a list item
 $('li').on('click', function (evt) {
-    hasClassOpen(evt);
-    // Prevent to open more than 2 cards and wait for preset timer
+    hasClassOpen(evt); // check if the clicked element is already opened before
+    // Prevent to open more than 2 cards, prevent to click on a prematched card, prevent to click on an already opened card
     if((openedCards.length<2) && !$(evt.target).hasClass('match') && !checkOpen){
-
-    // Checks whether this list item already has a match class or not.
       showCard(evt);
       addCardToArr(evt);
-      //wait untill totMatchedCtr is incremented
-      setTimeout(function(){
-        if (totMatchedCtr===16) {
-          alert("You win, your score is: "+ counter);
-        }
-      },900)
-  }
+      //wait untill matched cards counter is incremented
+      setTimeout(checkIfWin,900)
+    }
 })
 
 // Reset game
 $('.fa-repeat').on('click', function () {
   initialize();
 })
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
